f7f7beea666a9dc9e4b20934a1abc81acbf76f5b
John-AG <Johnandre9871@gmail.com>
2024-05-14 10:39:19 +0100

    Update models to include a User struct with ID, Name and Email properties

--- models/models.go
+++ models/models.go
@@ -3,3 +3,9 @@ package models
 type Response struct {
 	Message string `json:"message"`
 }
+
+type User struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
